11c911fd148f096d163c0987ed890b0c8ce86fa5
Justine Maoel <justinemaoel.gnc@gmail.com>
2026-06-25 18:14:44 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guagua National Colleges</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        
+    </nav>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
